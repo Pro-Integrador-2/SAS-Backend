9e4f2a983fa4b22d97d6e6abce22aaa1b12bb8dc
anvorja <borja.carlos@correounivalle.edu.co>
2024-04-23 11:51:36 -0500

    redentials on .env

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
 app = Flask(__name__)
 CORS(app)
 
